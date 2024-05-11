74d425d10407ea0132067b829e523edffcf140a4
nikhil <nikhiltelase@gmail.com>
2024-05-11 15:58:34 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,30 +1,59 @@
 import os
 
-# where to create folders
-path = "D:\My Code Base\connect shiksha learnings\Learnings"
+# where to create/edit folders
+path = "D:/My Code Base/connect shiksha learnings/Learnings"
 
-# files content
-html_content = """<h1>hellow world"""
-css_content = """h1 { color: red; }"""
-js_content = """console.log("hello world!")"""
+# CHANGING FOLDER NAME
+all_folders = os.listdir(path)
+# getting folder names list they include day-(1,2,3...)
+folder_names = []
+for i in range(21, 34):
+    for folder in all_folders:
+        new_name = folder.split("-")
+        # if folder not exits in folder_names than do this
+        if not folder in folder_names:
+            try:
+                if new_name[1] == "day":
+                    folder_names.append(folder)
+            except IndexError:
+                pass
+
+# List of want to name that folder
+renamed_folders = []
+for folder in folder_names:
+    renamed_folders.append(folder.replace(f"day-{folder_names.index(folder) + 1}-", ""))
+
+# Changing folders name
+for i in range(len(folder_names)):
+    folder_path = f"{path}/{folder_names[i]}"
+    new_folder_path = f"{path}/{renamed_folders[i]}"
+
+    os.rename(folder_path, new_folder_path)
 
 
-def generate_file(filename, content):
-    with open(filename, "w") as f:
-        f.write(content)
-
-
-for i in range(35, 41):
-    folder_name = i
-    # crating folder
-    folder_path = f"{path}/{folder_name}/task"
-    # os.mkdir(folder_path)
-
-    # creating files
-    html_file = f"{folder_path}/index.html"
-    css_file = f"{folder_path}/style.css"
-    js_file = f"{folder_path}/script.js"
-
-    # generate_file(html_file, html_content)
-    generate_file(css_file, css_content)
-    generate_file(js_file, js_content)
+# CRATING FOLDER AND FILES INSIDE FOLDER
+# # files content
+# html_content = """<h1>hellow world"""
+# css_content = """h1 { color: red; }"""
+# js_content = """console.log("hello world!")"""
+#
+#
+# def generate_file(filename, content):
+#     with open(filename, "w") as f:
+#         f.write(content)
+#
+#
+# for i in range(21, 34):
+#     folder_name = i
+#     # crating folder
+#     folder_path = f"{path}/{folder_name}/task"
+#     # os.mkdir(folder_path)
+#
+#     # creating files
+#     html_file = f"{folder_path}/index.html"
+#     css_file = f"{folder_path}/style.css"
+#     js_file = f"{folder_path}/script.js"
+#
+#     # generate_file(html_file, html_content)
+#     generate_file(css_file, css_content)
+#     generate_file(js_file, js_content)
