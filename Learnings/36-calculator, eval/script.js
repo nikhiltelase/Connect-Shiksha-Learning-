81d6890331b2e499da9fbf6060915daa8411d918
nikhil <nikhiltelase@gmail.com>
2024-05-31 19:06:29 +0530

    some updates

--- Learnings/36-calculator, eval/script.js
+++ Learnings/36-calculator, eval/script.js
@@ -1,1 +1,25 @@
-ww
+var input = document.querySelector("input");
+var buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    var btnText = e.target.innerText;
+
+    if (btnText === "AC") {
+      input.value = "";
+    } else if (btnText === "DEL") {
+      input.value = input.value.slice(0, -1);
+    }
+    else if(btnText === "="){
+      try {
+        input.value = eval(input.value)
+      } catch (error) {
+        alert("Invalid Expression")
+        input.value = ""
+      }
+    }
+    else {
+      input.value += btnText;
+    }
+  });
+});
