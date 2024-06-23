0d9990d8824d371043f06b081c73d0c41b6f6b88
nikhil <nikhiltelase@gmail.com>
2024-06-23 19:57:23 +0530

    revise react

--- Learnings/43-cards-routs/src/pages/Home.jsx
+++ Learnings/43-cards-routs/src/pages/Home.jsx
@@ -3,30 +3,30 @@ import Cards from '../componts/Card'
 import './home.css'
 
 function Home() {
-    function addCard() {
-        console.log('clicked')
-      }
-    
-    
-      return (
-        <>
-            <h1>Home Page</h1>
-          <div className="card-container">
-            <Cards name='ram' text='1' />
-            <Cards name='shym' text='2' />
-            <Cards name='ramnaresh' text='3' />
-            <Cards name='kanha' text='4' />
-            <Cards name='krishna' text='5' />
-            <Cards name='bharat' text='6' />
-            <Cards name='ramchnadra' text='7' />
-    
-          </div>
-    
-          {/* <div className='button'>
-            <button onClick={addCard}>Add Card</button>
-          </div> */}
-        </>
-      )
+  function addCard() {
+    console.log('clicked')
+  }
+
+
+  return (
+    <>
+      <h1>Home Page</h1>
+      <div className="card-container">
+        <Cards name='ram' text='1' />
+        <Cards name='shym' text='2' />
+        <Cards name='ramnaresh' text='3' />
+        <Cards name='kanha' text='4' />
+        <Cards name='krishna' text='5' />
+        <Cards name='bharat' text='6' />
+        <Cards name='ramchnadra' text='7' />
+
+      </div>
+
+      <div className='button'>
+        <button onClick={addCard}>Add Card</button>
+      </div>
+    </>
+  )
 }
 
 export default Home
