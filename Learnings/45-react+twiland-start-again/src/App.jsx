2cdb059b830aeada8bac7aa1e3d40797d66d5be4
nikhil <nikhiltelase@gmail.com>
2024-07-05 21:20:44 +0530

    react + twilaind

---
+++ Learnings/45-react+twiland-start-again/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Cards from './components/Cards'
+
+function App() {
+  return (
+    <div className='w-full flex flex-wrap'>
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+    </div>
+  )
+}
+
+export default App
---
+++ Learnings/45-react+twiland-start-again/src/components/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cards() {
+  return (
+    <div className='w-[330px] h-96 flex items-center justify-center rounded-3xl m-4 border-4 border-red-400'>
+      <h1 className='text-4xl opacity-70 '>Card</h1>
+    </div>
+  )
+}
+
+export default Cards
