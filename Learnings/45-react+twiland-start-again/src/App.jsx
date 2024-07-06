e6297b280cb0b995e876324ebdefc5cd4353fb79
nikhil <nikhiltelase@gmail.com>
2024-07-06 21:51:05 +0530

    work on ract card and props

--- Learnings/45-react+twiland-start-again/src/App.jsx
+++ Learnings/45-react+twiland-start-again/src/App.jsx
@@ -2,14 +2,37 @@ import React from 'react'
 import Cards from './components/Cards'
 
 function App() {
+  let users = [
+    {
+      name: 'Nikhil',
+      age: 21,
+      img:"https://img.freepik.com/free-photo/3d-render-little-boy-with-eyeglasses-blue-shirt_1142-50994.jpg?size=626&ext=jpg&ga=GA1.1.198045090.1710485532&semt=sph",
+      bgImg:"https://images.unsplash.com/photo-1720206811364-684e8f8e803f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+    },
+    {
+      name:'ram',
+      age:17,
+      img:"https://img.freepik.com/free-photo/3d-illustration-teenager-with-funny-face-glasses_1142-50955.jpg?size=626&ext=jpg&ga=GA1.1.198045090.1710485532&semt=sph",
+      bgImg: "https://images.unsplash.com/photo-1720211370947-68088964ae6b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0fHx8ZW58MHx8fHx8"
+    },
+    {
+      name: 'pintu',
+      age: 11,
+      img:"https://img.freepik.com/free-photo/androgynous-avatar-non-binary-queer-person_23-2151100226.jpg?size=626&ext=jpg&ga=GA1.1.198045090.1710485532&semt=sph",
+      bgImg: "https://images.unsplash.com/photo-1719844841024-3c7166816fc7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      name: 'aadi',
+      age: 21,
+      img:"https://img.freepik.com/free-photo/view-3d-man-holding-laptop_23-2150709818.jpg?size=626&ext=jpg&ga=GA1.1.198045090.1710485532&semt=sph",
+      bgImg: "https://images.unsplash.com/photo-1720068910279-6899141564bc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDh8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D"
+    },
+  ]
+
   return (
     <div className='w-full flex flex-wrap'>
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
+      <Cards users={users} />
+      
     </div>
   )
 }
--- Learnings/45-react+twiland-start-again/src/components/Cards.jsx
+++ Learnings/45-react+twiland-start-again/src/components/Cards.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import "./card.css";
 
-function Cards() {
+function Cards({ users }) {
   return (
-    <div className='w-[330px] h-96 flex items-center justify-center rounded-3xl m-4 border-4 border-red-400'>
-      <h1 className='text-4xl opacity-70 '>Card</h1>
-    </div>
-  )
+    <>
+      {users.map((user, index) => (
+        <div
+          key={index}
+          className="w-[330px] h-96 flex items-center justify-center rounded-3xl m-4 relative z-10 "
+          style={{
+            backgroundImage: `url(${user.bgImg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="flex justify-center items-center flex-col">
+            <img className="w-40 rounded-[50%] " src={user.img} alt="" />
+            <h1 className="text-4xl text-white capitalize">{user.name}</h1>
+            <h2 className=" text-white">age: {user.age}</h2>
+            <h3 className=" text-white">
+              {user.age >= 18
+                ? "You are eligible for vote."
+                : "You are not eligible for vote."}
+            </h3>
+          </div>
+          <div className="bg-black w-[330px] h-96 absolute -z-10 opacity-40  rounded-3xl"></div>
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default Cards
+export default Cards;
